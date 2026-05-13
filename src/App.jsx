10813b62ebc83a38ab1ce7cf2e7aa77b870fae32
Nurik-04 <alpisbayevnurdaulet@gmail.com>
2026-05-13 23:03:37 +0500

    Globus

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./App.css"
+
+const App = () => {
+  return (
+    <div>
+      <div className="globe">
+        <div className="globe__map"></div>
+        <div className="globe__map"></div>
+      </div>
+    </div>
+  )
+}
+
+export default App
