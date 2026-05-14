a4490bc9449b18c686147e47a1bd83ed59985d58
Nurik-04 <alpisbayevnurdaulet@gmail.com>
2026-05-14 22:03:39 +0500

    Header bolimi qoshildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from "./Components/Header/Header"
 import "./App.css"
 
 const App = () => {
   return (
     <div>
+      <Header />
       <div className="globe">
         <div className="globe__map"></div>
         <div className="globe__map"></div>
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import logo from "../../images/Logo.png";
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="container">
+                <nav className="nav">
+                    <div className="header__logo__box">
+                        <img src={logo} alt="logo" className="header__logo__img" />
+                        <h2 className='header__logo'>Globe</h2>
+                    </div>
+                    <ul>
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#about">Features</a></li>
+                        <li><a href="#contact">Pricing</a></li>
+                        <li><a href="#blog">Blog</a></li>
+                    </ul>
+                    <button className='header__btn'>Get Started</button>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header
